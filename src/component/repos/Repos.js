e94dd4f791597e4bb0aaabd542cd475323325844
chicong17 <dochicong.fbu@gmail.com>
2022-05-20 09:56:10 +0700

    reponsive done

--- src/component/repos/Repos.js
+++ src/component/repos/Repos.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import RepoItem from './RepoItem'
 import PropTypes from 'prop-types'
+import Grid from '@mui/material/Grid'
+import { experimentalStyled as styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper'
 
 const Repos = ({ repos }) => {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }))
   return (
-    <div style={userStyle}>
-      {repos && repos.map((repo) => <RepoItem repo={repo} key={repo.id} />)}
-    </div>
+    <Grid>
+      {repos &&
+        repos.map((repo) => (
+          <Grid item xs={12} md={12} lg={6}>
+            <Item>
+              <RepoItem repo={repo} key={repo.id} />
+            </Item>
+          </Grid>
+        ))}
+    </Grid>
   )
 }
 Repos.propTypes = {
   repos: PropTypes.array.isRequired
 }
-const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(2, 1fr)',
-  gridGap: '1rem'
-}
+
 export default Repos
--- src/component/users/Users.js
+++ src/component/users/Users.js
@@ -2,26 +2,37 @@ import React, { useContext } from 'react'
 import UserItem from './UserItem'
 import Spinner from '../layout/Spinner'
 import githubContext from '../../context/github/githubContext'
+import Grid from '@mui/material/Grid'
+import { experimentalStyled as styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper'
 
 const Users = () => {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }))
   const gc = useContext(githubContext)
   const { loading, users } = gc.state
   if (loading) {
     return <Spinner />
   } else {
     return (
-      <div style={userStyle}>
+      <Grid container spacing={{ sm: 1, md: 1, lg: 1 }}>
         {users.length
-          ? users.map((user) => <UserItem key={user.id} user={user} />)
+          ? users.map((user) => (
+              <Grid item xs={12} md={6} lg={4}>
+                <Item>
+                  <UserItem key={user.id} user={user} />
+                </Item>
+              </Grid>
+            ))
           : true}
-      </div>
+      </Grid>
     )
   }
-}
-const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
 }
 
 export default Users
